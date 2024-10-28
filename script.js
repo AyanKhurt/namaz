c03589be3e7010f83e1b2a03ebbc8674ee21ebe6
AyanKhurt <ayankhurt@gmail.com>
2024-10-28 10:43:08 +0500

    new

---
+++ script.js
@@ -0,0 +1,17 @@
+function namaz() {
+    let userInput1 = document.getElementById("input1").value;
+    let userInput2 = document.getElementById("input2").value;
+
+  
+    axios.get(`https://api.aladhan.com/v1/timingsByCity?city=${userInput1}&country=${userInput2}`)
+        .then(function(response) {
+            let namaz = response.data;
+            console.log(namaz)
+            document.getElementById("namaz1").innerHTML = `<h2>fajar: ${namaz.data.timings.Fajr}`;
+            console.log(namaz.data.timings.Fajr)
+
+        })
+        .catch(function(err) {
+            document.getElementById("namaz1").innerHTML = `<p>Error</p>`;
+        });
+  }
